c403cdbbd3ac202038671b3337a2bcd62cdb17d7
YuriyOrlov <navuchodonsr@yandex.ru>
2017-01-26 09:47:22 +0300

    renamed some variables

--- bars.py
+++ bars.py
@@ -25,19 +25,20 @@ def get_closest_bar(data, longitude, latitude):
         bar_coords = item.get('geoData').get('coordinates')
         bar_latitude, bar_longitude = [float(item) for item in bar_coords]
         distance_from_user = float(sqrt((bar_longitude - longitude)**2 + (bar_latitude - latitude)**2))
-        if closest_bar >= distance_from_user or closest_bar is 0:
+        if closest_bar >= distance_from_user or not closest_bar:
             closest_bar = distance_from_user
             closest_bar_name = item.get('Name')
     return closest_bar_name
 
 
 if __name__ == '__main__':
-    data = load_data("./bars.json")
-    print("\nThe biggest bar is: {}".format(get_biggest_bar(data)))
-    print("\nThe smallest bar is: {}".format(get_smallest_bar(data)))
+    bars_from_file = load_data("./bars.json")
+    print("\nThe biggest bar is: {}".format(get_biggest_bar(bars_from_file)))
+    print("\nThe smallest bar is: {}".format(get_smallest_bar(bars_from_file)))
     user_coords = input('\nEnter your coordinates ("longitude;latitude") using semicolon(";") as a splitter> ')
     if ';' in user_coords:
         user_coords = [float(item.strip().replace(',', '.')) for item in user_coords.split(';')]
     else:
         raise IOError("Invalid coordinates!")
-    print("\nThe closest bar is: {}".format(get_closest_bar(data, longitude=user_coords[0], latitude=user_coords[1])))
+    print("\nThe closest bar is: {}".format(get_closest_bar(bars_from_file,
+                                                            longitude=user_coords[0], latitude=user_coords[1])))
