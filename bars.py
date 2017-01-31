7c8c56ae7f0ed23a0651d54a5d6c1c0b6606e9d8
YuriyOrlov <navuchodonsr@yandex.ru>
2017-01-31 17:34:07 +0300

    Fixed functions and made them pure.

--- bars.py
+++ bars.py
@@ -18,32 +18,31 @@ def get_smallest_bar(data):
     return min(data, key=lambda x: x['SeatsCount'])['Name']
 
 
-def receiving_user_coordinates():
-    user_coords = input('\nEnter your coordinates ("longitude;latitude") using semicolon(";") as a splitter> ')
-    if ';' in user_coords:
-        return [float(item.strip().replace(',', '.')) for item in user_coords.split(';')]
+def receive_user_coordinates(data):
+    if ';' in data:
+        return [float(item.strip().replace(',', '.')) for item in data.split(';')]
     else:
         raise ValueError("Invalid coordinates!")
 
 
 def show_closest_bar(lat_lng_list):
-    return "\nThe closest bar is: {}".format(calculate_closest_bar(bars_from_file,
-                                                                   longitude=lat_lng_list[0],
-                                                                   latitude=lat_lng_list[1]))
+    return calculate_closest_bar(bars_from_file, longitude=lat_lng_list[0], latitude=lat_lng_list[1])
 
 
-def distance_from_user(user_lat, user_lon, dot_coords):
+def calculate_distance_from_user(user_lat, user_lon, dot_coords):
     return float(sqrt((dot_coords[1] - user_lon)**2 + (dot_coords[0] - user_lat)**2))
 
 
 def calculate_closest_bar(data, longitude, latitude):
-    return min(data, key=lambda x: distance_from_user(user_lon=longitude,
-                                                      user_lat=latitude,
-                                                      dot_coords=x['geoData']['coordinates']))['Name']
+    return min(data, key=lambda x: calculate_distance_from_user(user_lon=longitude,
+                                                                user_lat=latitude,
+                                                                dot_coords=x['geoData']['coordinates']))['Name']
 
 
 if __name__ == '__main__':
     bars_from_file = load_data("./bars.json")
     print("\nThe biggest bar is: {}".format(get_biggest_bar(bars_from_file)))
     print("\nThe smallest bar is: {}".format(get_smallest_bar(bars_from_file)))
-    print(show_closest_bar(receiving_user_coordinates()))
+    user_coords_string = input('\nEnter your coordinates ("longitude;latitude") using semicolon(";") as a splitter> ')
+    user_coords_list = receive_user_coordinates(user_coords_string)
+    print("\nThe closest bar is: {}".format(show_closest_bar(user_coords_list)))
